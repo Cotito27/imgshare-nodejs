439b3a6076280e213a4089546cef897fa37862ea
Cotito27 <65867363+Cotito27@users.noreply.github.com>
2020-09-13 00:10:41 -0500

    first version

---
+++ src/controllers/auth.js
@@ -0,0 +1,35 @@
+const passport = require("passport");
+
+const ctrl = {};
+
+ctrl.renderSignUp = (req, res) => {
+  res.render("authentication/signup", {
+    layout: "nostats",
+  });
+};
+
+ctrl.renderSignIn = (req, res) => {
+  console.log('xd');
+  res.render("authentication/signin", {
+    layout: "nostats",
+  });
+};
+
+ctrl.signUp = passport.authenticate("signup", {
+  successRedirect: "/signin",
+  failureRedirect: "/signup",
+  failureFlash: true,
+});
+
+ctrl.signIn = passport.authenticate("signin", {
+  successRedirect: "/",
+  failureRedirect: "/signin",
+  failureFlash: true,
+});
+
+ctrl.logout = (req, res) => {
+  req.logout();
+  res.redirect("/");
+};
+
+module.exports = ctrl;
